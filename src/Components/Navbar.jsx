271dc089799e96c1825594e0ebdc4169553f5479
Mrityunjay-Kumar12 <120048231+Mrityunjay-Kumar12@users.noreply.github.com>
2025-11-18 12:01:32 +0530

    home

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,46 +6,85 @@ export default function Navbar() {
     <>
       <Topbar />
 
-      <nav className="navbar container-fluid">
-        <div className="logo">
-          <h2>
-            Arc<span>Hotel</span>
-          </h2>
+      <nav className="navbar navbar-expand-lg custom-nav">
+        <div className="container-fluid">
+          {/* Logo */}
+          <a className="navbar-brand text-warning fw-bold" href="/">
+            <h2 className="m-0">
+              Arc <span className="text-light">Hotel</span>
+            </h2>
+          </a>
+
+          {/* Toggle Button */}
+          <button
+            className="navbar-toggler custom-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          {/* Menu */}
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link text-warning" href="/">
+                  Home
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link text-warning" href="/about">
+                  About
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link text-warning" href="/rooms">
+                  Rooms
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link text-warning" href="/services">
+                  Services
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link text-warning" href="/contact">
+                  Contact
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link text-warning" href="/event">
+                  Event
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link text-warning" href="/menu">
+                  Menu
+                </a>
+              </li>
+            </ul>
+
+            {/* Buttons */}
+            <div className="d-flex gap-2">
+              <a href="/bookingform" className="btn btn-orange">
+                Book Now
+              </a>
+              <a href="/login" className="btn btn-yellow">
+                Login Now
+              </a>
+            </div>
+          </div>
         </div>
-
-        <ul className="nav-links">
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/rooms">Rooms</a>
-          </li>
-          <li>
-            <a href="/services">Services</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-          <li>
-            <a href="/event">Event</a>
-          </li>
-          <li>
-            <a href="/menu">Menu</a>
-          </li>
-        </ul>
-
-        <button className="book-btn">
-          {" "}
-          <a href="/bookingform">Book Now</a>
-        </button>
-        
-        <button className="book-btn">
-          {" "}
-          <a href="/login">Login Now</a>
-        </button>
       </nav>
     </>
   );
